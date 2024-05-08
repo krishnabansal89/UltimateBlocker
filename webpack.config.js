16bd58cacd17582bdbbae3e93dd890b9860a8807
Krishna Bansal <krishnabansal890@gmail.com>
2024-05-09 00:18:13 +0530

    webpack config file added

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    popup: './src/popup.jsx',
+    background: './src/background.js',
+    content: './src/content.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'public', to: '.' },
+        { from: 'src/manifest.json', to: '.' },
+      ],
+    }),
+  ],
+};
