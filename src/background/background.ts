618d44b235197961f2474cc3afa1e14ce2b73c69
Krishna Bansal <krishnabansal890@gmail.com>
2024-05-17 15:03:35 +0530

    Incgonito Mode Checked

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,12 +1,26 @@
 
 console.log("Bakcground Script Loaded!")
-export var isAllowed = false; 
+
 const privateAllowed = async ()=>{
     const data = await chrome.extension.isAllowedIncognitoAccess()
     console.log(data)
     chrome.storage.local.set({isAllowed: data}, ()=>{
         console.log("Data Saved")
     })
+    if(data!==true)
+        {
+            chrome.tabs.query({active: true, currentWindow: true}, (tabs)=>{
+                if(tabs.length>0){
+                    const activeTab = tabs[0]
+                    if(!activeTab.url?.includes("://extensions/")){
+                    // chrome.tabs.sendMessage(activeTab.id,{data:"privateNotAllowed"},(response)=>{
+                    //     console.log("Repsone from ContentScript")
+                    // })
+                    chrome.tabs.create({url:"chrome://extensions/"},(tab)=>{
+                        console.log(tab)
+                    })
+                }}
+        })}
 }
 setInterval(privateAllowed, 1000)
 chrome.runtime.setUninstallURL( 
